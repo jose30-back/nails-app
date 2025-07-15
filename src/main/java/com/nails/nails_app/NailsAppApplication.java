7f898760e1b1c9d23a628e75644ebf884a96d277
José Manuel Arango <josemanuelarango@macbook-pro-de-jose.home>
2025-07-16 01:31:33 +0200

    primer commit - estructura base del proyecto

---
+++ src/main/java/com/nails/nails_app/NailsAppApplication.java
@@ -0,0 +1,13 @@
+package com.nails.nails_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NailsAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NailsAppApplication.class, args);
+	}
+
+}
