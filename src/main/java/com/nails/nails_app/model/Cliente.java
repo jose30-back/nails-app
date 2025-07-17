9fe89ab5fcd7ab25371e34fc28d9ec6a13ae4d7e
José Manuel Arango <josemanuelarango@macbook-pro-de-jose.home>
2025-07-18 01:36:29 +0200

    feat: crear entidad Cliente con anotaciones JPA

---
+++ src/main/java/com/nails/nails_app/model/Cliente.java
@@ -0,0 +1,60 @@
+package com.nails.nails_app.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Cliente {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String phone;
+    private String email;
+
+    public Cliente() {
+    }
+
+    public Cliente(Long id, String name, String phone, String email) {
+        this.id = id;
+        this.name = name;
+        this.phone = phone;
+        this.email = email;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
